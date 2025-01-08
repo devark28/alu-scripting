8b6fb76bd0ade034a1205fdd005bec3674060d5e
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-08 22:37:43 +0200

    Update 0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-ARGV.each do |arg|
-  matches = arg.scan(/School/)
-  puts matches.join if matches.any?
-end
+# ARGV.each do |arg|
+#   matches = arg.scan(/School/)
+#   puts matches.join if matches.any?
+# end
+puts ARGV[0].scan(/School/).join
