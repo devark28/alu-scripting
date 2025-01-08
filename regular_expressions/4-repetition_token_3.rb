ab653a37372c4be1eac11967344c5dc2f45af94b
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-08 22:41:25 +0200

    Update 4-repetition_token_3.rb

--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
-ARGV.each do |arg|
-  if arg.match?(/hbt+n/)
-    puts arg
-  else
-    puts ''
-  end
-end
+# ARGV.each do |arg|
+#   if arg.match?(/hbt+n/)
+#     puts arg
+#   else
+#     puts ''
+#   end
+# end
+puts arg if if ARGV[0].match?(/hbt+n/g) else ''
