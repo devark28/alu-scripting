7563bf4af1095a7be1dd2589cd5b9ed4e0197458
devark28 <e.shimwa1@alustudent.com>
2025-01-09 14:51:02 +0200

    Chore: clean up unnecessary code

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-# ARGV.each do |arg|
-#   if arg.match?(/hbt{2,5}n/)
-#     puts arg
-#   else
-#     puts ''
-#   end
-# end
 puts ARGV[0].scan(/hbt{2,5}n/).join
--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-# ARGV.each do |arg|
-#   if arg.match?(/hbt+n/)
-#     puts arg
-#   else
-#     puts ''
-#   end
-# end
 puts ARGV[0].scan(/hbt+n/).join
