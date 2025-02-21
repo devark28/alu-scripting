4dad8f560511af876c381eb20dc0ff0e40acbf4f
devark28 <e.shimwa1@alustudent.com>
2025-02-21 14:20:21 +0200

    add print to see what reddit they are checking

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ import requests
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
+    print(subreddit)
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
