c25f42346e7a39ecac09731b45200f5fb2a15494
devark28 <e.shimwa1@alustudent.com>
2025-02-21 15:22:06 +0200

    Fix: task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Chrome/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     # print(response)
-    print(response.text[:1000])
+    print(response.text[1000:])
     if response.status_code != 200:
         print(None)
     else:
