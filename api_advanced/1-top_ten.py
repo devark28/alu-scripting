505fdbd96f693d0d954cc1588cef817568e9a37f
devark28 <e.shimwa1@alustudent.com>
2025-02-21 15:24:54 +0200

    Fix: task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,8 +8,8 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Chrome/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    print(response)
-    print(response.text[2000:])
+    # print(response)
+    print(response.text[5000:])
     if response.status_code != 200:
         print(None)
     else:
